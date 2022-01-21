00ba2029d682e8990d753344c7af2db8fb3c5103
payeplionel <payeplionel2@outlook.com>
2022-01-21 20:42:01 +0100

    updates

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static('./dist/fonetApp'));
+app.use(express.static('./dist/om'));
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/fonetApp/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/om/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
