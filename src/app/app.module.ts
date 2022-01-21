402d64bad94674935e6f382d64878f958de21fd6
payeplionel <payeplionel2@outlook.com>
2022-01-21 16:22:31 +0100

    commit all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IndexComponent } from './index/index.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/index/index.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { RetraitserviceService } from '../retraitservice.service';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+export class IndexComponent implements OnInit {
+  option = "Orange Money";
+  frais = 0;
+  fraisc = 0;
+  values = 0;
+  retrait: any;
+  retraitc: any;
+  nombreRetrait = 0;
+  tabRetrait: any;
+  attention = "";
+  concurence = "";
+
+
+
+  constructor(private retraiservice: RetraitserviceService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  changeom() {
+    this.option = "Orange Money";
+   
+  }
+
+  changemomo() {
+    this.option = "Mobile Money";
+  }
+
+  onKey(event: any) {
+    if ((event.target.value >= 100) && (event.target.value % 5 == 0) && (this.isInt(event.target.value) == true)&&(event.target.value <= 500000)) {
+      if (this.option == "Orange Money") {
+        let resp = this.retraiservice.getRetrait("om", event.target.value);
+        resp.subscribe(data => {
+          this.retrait = data;
+          this.frais = this.retrait.frais;
+          this.nombreRetrait = this.retrait.sommes.length;
+          this.tabRetrait = this.retrait.sommes;
+        });
+
+        let res = this.retraiservice.getRetrait("momo", event.target.value);
+        res.subscribe(data => {
+          this.retraitc = data;
+          this.fraisc = this.retraitc.frais;
+         
+          if (this.fraisc < this.frais) {
+
+            this.concurence = "Mobile money propose des frais à " + this.fraisc + " pour le même montant";
+          }
+          else {
+            this.concurence = "";
+          }
+        });
+
+
+        this.attention = "";
+      }
+      else if ((this.option == "Mobile Money") && (event.target.value % 5 == 0) && (this.isInt(event.target.value) == true) &&(event.target.value <= 500000)) {
+        let resp = this.retraiservice.getRetrait("momo", event.target.value);
+        resp.subscribe(data => {
+          this.retrait = data;
+          this.frais = this.retrait.frais;
+          this.nombreRetrait = this.retrait.sommes.length;
+          this.tabRetrait = this.retrait.sommes;
+
+        });
+
+        let res = this.retraiservice.getRetrait("momo", event.target.value);
+        res.subscribe(data => {
+          this.retraitc = data;
+          this.fraisc = this.retraitc.frais;
+         
+          if (this.fraisc < this.frais) {
+
+            this.concurence = "Orange money propose des frais à " + this.fraisc + " pour le même montant";
+          }
+          else {
+            this.concurence = "";
+          }
+        });
+
+        this.attention = "";
+      }
+    }
+    else if ((this.isInt(event.target.value) == false) || (event.target.value % 5 != 0) || (event.target.value > 500000)) {
+      this.attention = "Nb : La sommes doit respecter le système monétaire du Cameroun et doit être en chiffre et être inférieur à 500.000 xaf";
+    }
+    else {
+      this.nombreRetrait = 0;
+      this.frais = 0;
+    }
+  }
+
+  isInt(value: any) {
+    var er = /^-?[0-9]+$/;
+    return er.test(value);
+  }
+
+
+}
