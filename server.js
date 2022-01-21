c1e2e9c5452e9d51ca2b27c6a292c00bc43b7fb7
payeplionel <payeplionel2@outlook.com>
2022-01-21 21:05:20 +0100

    updates

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
-
 const app = express();
 
-// add this code
-const whitelist = ['https://dashboard.heroku.com']; // list of allow domain
+const path = require('path');
+const cors = require('cors');
+app.use(cors());
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
+app.use(express.static(__dirname+'/dist/om'));
 
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-// end 
-app.use(cors(corsOptions));
-
-app.use(express.static('./dist/om'));
-
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/om/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
