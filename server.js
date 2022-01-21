e213a12c61436a46109d9bb8adf0d004480771af
payeplionel <payeplionel2@outlook.com>
2022-01-21 19:57:51 +0100

    updates

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
-    next();
-});
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
 
 const path = require('path');
 const app = express();
