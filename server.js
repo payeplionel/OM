d266ae82af1192552d2a3f6789640f4fe09d0af1
payeplionel <payeplionel2@outlook.com>
2022-01-21 20:25:41 +0100

    updates

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const cors = require('cors');
 
 var app = express()
 
-
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(express.static(__dirname + '/dist/om'));
-app.get('/*', function(req,res) {
+app.get('/*', cors(), function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/om/index.html'));});
 app.listen(process.env.PORT || 8080);
