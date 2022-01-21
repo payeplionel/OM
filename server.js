67582b12ecee22c7ef97358eb31c0f5df8bef879
payeplionel <payeplionel2@outlook.com>
2022-01-21 19:53:15 +0100

    updates

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+    next();
+});
+
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/om'));
