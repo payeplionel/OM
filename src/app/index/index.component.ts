c716a1fccd260534464e8ce00e01fcb1e6b1b08c
payeplionel <payeplionel2@outlook.com>
2022-01-28 09:21:52 +0100

    updates views

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -20,6 +20,8 @@ export class IndexComponent implements OnInit {
   attention = "";
   concurence = "";
   selected = 'om';
+  inpt='';
+  gain='';
 
 
   constructor(private retraiservice: RetraitserviceService) {
@@ -31,29 +33,44 @@ export class IndexComponent implements OnInit {
 
   changeom() {
     this.option = "Orange Money";
-   
+    this.attention = "";
+    this.concurence = "";
+    this.nombreRetrait=0;
+    this.inpt='';
+    this.frais=0;
+    this.gain='';
+
   }
 
   changemomo() {
     this.option = "Mobile Money";
+    this.attention = "";
+    this.concurence = "";
+    this.nombreRetrait=0;
+    this.inpt='';
+    this.frais=0;
+    this.gain='';
+
   }
 
   onKey(event: any) {
-    if ((event.target.value >= 100) && (event.target.value % 5 == 0) && (this.isInt(event.target.value) == true)&&(event.target.value <= 500000)) {
+    if ((event.target.value >= 100) && (event.target.value % 5 == 0) && (this.isInt(event.target.value) == true) && (event.target.value <= 500000)) {
       if (this.option == "Orange Money") {
         let resp = this.retraiservice.getRetrait("om", event.target.value);
         resp.subscribe(data => {
           this.retrait = data;
           this.frais = this.retrait.frais;
+          console.log(data);
           this.nombreRetrait = this.retrait.sommes.length;
           this.tabRetrait = this.retrait.sommes;
+          this.gain = "Les frais initiaux étaient de : "+this.retrait.initial;
         });
 
-        let res = this.retraiservice.getRetrait("momo", event.target.value);
+        /*let res = this.retraiservice.getRetrait("momo", event.target.value);
         res.subscribe(data => {
           this.retraitc = data;
           this.fraisc = this.retraitc.frais;
-         
+
           if (this.fraisc < this.frais) {
 
             this.concurence = "Mobile money propose des frais à " + this.fraisc + " pour le même montant";
@@ -61,26 +78,26 @@ export class IndexComponent implements OnInit {
           else {
             this.concurence = "";
           }
-        });
-
+        });*/
 
         this.attention = "";
       }
-      else if ((this.option == "Mobile Money") && (event.target.value % 5 == 0) && (this.isInt(event.target.value) == true) &&(event.target.value <= 500000)) {
+      else if ((this.option == "Mobile Money") && (event.target.value % 5 == 0) && (this.isInt(event.target.value) == true) && (event.target.value <= 500000)) {
         let resp = this.retraiservice.getRetrait("momo", event.target.value);
         resp.subscribe(data => {
           this.retrait = data;
           this.frais = this.retrait.frais;
           this.nombreRetrait = this.retrait.sommes.length;
           this.tabRetrait = this.retrait.sommes;
+          this.gain = "Les frais initiaux étaient de : "+this.retrait.initial;
 
         });
 
-        let res = this.retraiservice.getRetrait("momo", event.target.value);
+        /*let res = this.retraiservice.getRetrait("momo", event.target.value);
         res.subscribe(data => {
           this.retraitc = data;
           this.fraisc = this.retraitc.frais;
-         
+
           if (this.fraisc < this.frais) {
 
             this.concurence = "Orange money propose des frais à " + this.fraisc + " pour le même montant";
@@ -88,17 +105,21 @@ export class IndexComponent implements OnInit {
           else {
             this.concurence = "";
           }
-        });
+        });*/
 
         this.attention = "";
       }
     }
     else if ((this.isInt(event.target.value) == false) || (event.target.value % 5 != 0) || (event.target.value > 500000)) {
       this.attention = "Nb : La sommes doit respecter le système monétaire du Cameroun et doit être en chiffre et être inférieur à 500.000 xaf";
+      this.nombreRetrait = 0;
+      this.frais = 0;
+      this.gain='';
     }
     else {
       this.nombreRetrait = 0;
       this.frais = 0;
+      this.gain='';
     }
   }
 
